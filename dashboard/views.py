759cd3e846b0d459391d2633bab95d678e12ccc5
Anton Sorokoumov <sorokoumov.anton@gmail.com>
2019-07-05 18:26:10 +0300

    Filter carriers only with sender region

--- dashboard/views.py
+++ dashboard/views.py
@@ -99,7 +99,6 @@ def index(request):
 def carriers(request, order_id):
     order = Orders.objects.get(id=order_id)
     similar_orders = Orders.objects.filter(sender_region_id=order.sender_region_id,
-                                           recipient_region_id=order.recipient_region_id,
                                            created_at__gte=datetime.date(2019, 1, 1),
                                            cargo_kind__in = ['metal', 'other'])
 
